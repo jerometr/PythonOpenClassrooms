17eec810c12b94ac998bf592b362c95895e2c480
floinphb <57228594+floinphb@users.noreply.github.com>
2025-10-13 20:32:46 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+#Définissez une variable nom contenant votre nom sous forme de chaîne de caractères.
+nom = "Florentin" 
+#Définissez une variable age contenant votre âge sous forme d'un entier.
+age = 35
+#Définissez une variable taille contenant votre taille sous forme d'un nombre à virgule flottante.
+taille = 1.70
+#Définissez une variable est_etudiant contenant une valeur booléenne et indiquant si vous êtes étudiant ou non.
+est_etudiant = 'oui'
+#Utilisez la fonction print pour afficher les valeurs des variables précédemment définies.
+print(f"Je me nomme {nom}, j'ai {age} ans, ma taille est {taille} et je suis etudiant {est_etudiant}")
+print("Type nom:",type(nom))
+print("Type age:",type(age))
+print("Type taille:",type(taille))
+print("Type est_etudiant:",type(est_etudiant))
