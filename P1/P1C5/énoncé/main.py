fb1b041c20dca7d07dc4802963f4bb51ffea52db
christbobo <bujitudev@gmail.com>
2025-11-12 23:07:40 +0100

    ajout des variables et affiche des types

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+nom = " christian"
+age = 25 
+taille = 1.69
+est_etudiant = True
+print(nom)
+print(age)
+print(taille)
+print(est_etudiant)
+print(type(nom))
+print(type(age))
+print(type(taille))
+print(type(est_etudiant))
