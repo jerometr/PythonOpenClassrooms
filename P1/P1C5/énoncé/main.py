f04aa321b36993bdff1ace1f7fe66986cd5fdb97
christbobo <bujitudev@gmail.com>
2025-11-14 01:04:09 +0100

    ajout des variables et affichage des types
    j'ai ajouté les variables demandées et j'ai affiché leur type avec la fonction type().

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+nom = "christian"
+age = 25
+taille = 1.69
+est_etudiant = False
+print("NOM:",nom)
+print("AGE:",age)
+print("TAILLE:",taille)
+print("EST ETUDIANT:",est_etudiant)
+print("type nom:",type(nom))
+print("type age:",type(age))
+print("type taille:",type(taille))
+print("type est etudiant:",type(est_etudiant))
