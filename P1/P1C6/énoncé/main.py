6e0962892799d4e1f5362c6c232be8323f31463b
christbobo <bujitudev@gmail.com>
2025-11-18 00:22:21 +0100

    Modify fruit list with additions and modifications
    ajout de kiwi, suppression d'orange, modification de banane en ananas, tri et affichages de la longueur.

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+fruits = ["pomme", "banana","orange"]
+fruits.append("kiwi")
+fruits.remove("orange")
+fruits[1] = "ananas"
+print(len(fruits))
+fruits.sort()
+print(fruits)
