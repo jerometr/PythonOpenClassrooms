497e6de2fdb7dadc625f0c0782fa7d3d002aff43
christbobo <bujitudev@gmail.com>
2025-11-17 22:22:05 +0100

    ajout des variables et affichages de leurs valeurs
    création des variables nom ,age ,taille ,et est_etudiant, puis utilisation de print pour afficher les valeurs et leurs type

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,13 @@
-# Écrivez votre code ici !
+nom = "christian"
+age = 25
+taille = 1.69
+est_etudiant = False
+print(nom)
+print(age)
+print(taille)
+print(est_etudiant)
+print(nom,type(nom))
+print(age,(type(age))
+print(taille,(type(taille))
+print(est_etudiant,type(est_etudiant)
+      
