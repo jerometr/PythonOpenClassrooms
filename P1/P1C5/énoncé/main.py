93547d05859d385734f33f84292c78cb1056a776
jerometr <63811251+jerometr@users.noreply.github.com>
2025-12-30 22:39:33 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+nom = "Triomphe"
+age = 55
+taille = 1.74
+est_etudiant = false
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"Est étudiant: {est_etudiant}")
+print(f"Type nom: {type(nom)}"
+print(f"Type age: {type(age)}"
+print(f"Type taille: {type(taille)}"
+print(f"Type est_etudiant: {type(est_etudiant)}"
