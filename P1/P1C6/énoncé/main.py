3f9ee6d322fccfdb9d6a4cf06359a671064f66df
jerometr <63811251+jerometr@users.noreply.github.com>
2025-12-30 23:16:37 +0100

    Fix fruit removal and sorting methods

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,7 +1,7 @@
 fruits = ["pomme", "banane","orange"]
 fruits.append("kiwi")
-remove(fruits[2])
+fruits.remove(fruits[2])
 fruits[1] = "ananas"
 len(fruits)
-sort(fruits)
+fruits.sort()
 print(fruits)
