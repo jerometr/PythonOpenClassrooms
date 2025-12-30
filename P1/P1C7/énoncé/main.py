0323f7977b9e5996cfe3de8453dc8225a2f2680d
jerometr <63811251+jerometr@users.noreply.github.com>
2025-12-30 23:41:46 +0100

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,9 @@
-# Écrivez votre code ici !
+fruits=dict()
+fruits["pomme"] = "rouge"
+fruits["banane"] = "jaune"
+fruits["orange"] = "orange"
+fruits["kiwi"] = "vert"
+couleur_banane = fruits["banane"]
+fruits["pomme"] = "vert"
+fruits.remove("banane")
+print(fruits.keys())
