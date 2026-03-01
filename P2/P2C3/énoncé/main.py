3e9173c0cf221aed1db8258193649cf5ea7730b8
jerometr <63811251+jerometr@users.noreply.github.com>
2026-03-01 17:35:23 +0100

    Add functions to calculate weekly and hourly salary

--- P2/P2C3/énoncé/main.py
+++ P2/P2C3/énoncé/main.py
@@ -6,13 +6,24 @@ def salaire_mensuel(salaire_annuel)
   return salaire_mensuel
   
 #    Créez une fonction appelée  salaire_hebdomadaire(salaire_mensuel)  qui prend en paramètre un salaire mensuel et retourne le salaire hebdomadaire correspondant en divisant le salaire mensuel par 4.
-
+def salaire_hebdomadaire(salaire_mensuel)
+  salaire_hebdomadaire = salaire_mensuel / 4
+  return salaire_hebdomadaire
+  
 #    Créez une fonction appelée  salaire_horaire(salaire_hebdomadaire, heures_travaillees)  qui prend en paramètres un salaire hebdomadaire et le nombre d'heures travaillées par semaine, et retourne le salaire horaire correspondant en divisant le salaire hebdomadaire par le nombre d'heures travaillées par semaine.
+def salaire_horaire(salaire_hebdomadaire, heures_travaillees)
+  if heures_travaillées != 0
+  salaire_horaire = salaire_hebdomadaire / heures_travaillees
+  else
+  salaire_horaire = 0
 
 #    Demandez à l'utilisateur de saisir son salaire annuel.
+salaire_annuel = input ('Salaire annuel :')
 
 #    Demandez à l'utilisateur de saisir le nombre d'heures travaillées par semaine.
+heures_travaillees = input ('Nombre d'heures travaillees par semaine :')
 
 #    Appelez les fonctions précédemment créées pour calculer le salaire horaire correspondant.
+print(f"Votre salaire horaire est de {salaire_horaire(salaire_hebdomadaire(salaire_annuel),heures_travaillees)} euros")
 
 #    Affichez le résultat sous la forme : "Votre salaire horaire est de XX euros".Ecrivez votre code ici
