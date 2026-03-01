8377367c6fc30c68fe8e793fdb27723a0b1474d2
jerometr <63811251+jerometr@users.noreply.github.com>
2026-03-01 17:45:03 +0100

    Fix function definitions and input handling in main.py

--- P2/P2C3/énoncé/main.py
+++ P2/P2C3/énoncé/main.py
@@ -1,27 +1,28 @@
 # Consignes
 
 #    Créez une fonction appelée  salaire_mensuel(salaire_annuel)  qui prend en paramètre un salaire annuel et retourne le salaire mensuel correspondant en divisant le salaire annuel par 12.
-def salaire_mensuel(salaire_annuel)
+def salaire_mensuel(salaire_annuel):
   salaire_mensuel = salaire_annuel / 12
   return salaire_mensuel
   
 #    Créez une fonction appelée  salaire_hebdomadaire(salaire_mensuel)  qui prend en paramètre un salaire mensuel et retourne le salaire hebdomadaire correspondant en divisant le salaire mensuel par 4.
-def salaire_hebdomadaire(salaire_mensuel)
+def salaire_hebdomadaire(salaire_mensuel):
   salaire_hebdomadaire = salaire_mensuel / 4
   return salaire_hebdomadaire
   
 #    Créez une fonction appelée  salaire_horaire(salaire_hebdomadaire, heures_travaillees)  qui prend en paramètres un salaire hebdomadaire et le nombre d'heures travaillées par semaine, et retourne le salaire horaire correspondant en divisant le salaire hebdomadaire par le nombre d'heures travaillées par semaine.
-def salaire_horaire(salaire_hebdomadaire, heures_travaillees)
-  if heures_travaillées != 0
+def salaire_horaire(salaire_hebdomadaire, heures_travaillees):
+  if heures_travaillees != 0
   salaire_horaire = salaire_hebdomadaire / heures_travaillees
   else
   salaire_horaire = 0
+  return salaire_horaire
 
 #    Demandez à l'utilisateur de saisir son salaire annuel.
-salaire_annuel = input ('Salaire annuel :')
+salaire_annuel = float(input ('Salaire annuel :'))
 
 #    Demandez à l'utilisateur de saisir le nombre d'heures travaillées par semaine.
-heures_travaillees = input ('Nombre d'heures travaillees par semaine :')
+heures_travaillees = float(input ('Nombre d'heures travaillees par semaine :'))
 
 #    Appelez les fonctions précédemment créées pour calculer le salaire horaire correspondant.
 print(f"Votre salaire horaire est de {salaire_horaire(salaire_hebdomadaire(salaire_annuel),heures_travaillees)} euros")
