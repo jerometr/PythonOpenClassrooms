f7ff4e85652290977a334eb1e791a8121a2ce274
jerometr <63811251+jerometr@users.noreply.github.com>
2026-03-01 17:26:02 +0100

    Implement salary calculation functions
    Added functions to calculate monthly, weekly, and hourly salary based on annual input.

--- P2/P2C3/énoncé/main.py
+++ P2/P2C3/énoncé/main.py
@@ -1,1 +1,18 @@
-# Ecrivez votre code ici
+# Consignes
+
+#    Créez une fonction appelée  salaire_mensuel(salaire_annuel)  qui prend en paramètre un salaire annuel et retourne le salaire mensuel correspondant en divisant le salaire annuel par 12.
+def salaire_mensuel(salaire_annuel)
+  salaire_mensuel = salaire_annuel / 12
+  return salaire_mensuel
+  
+#    Créez une fonction appelée  salaire_hebdomadaire(salaire_mensuel)  qui prend en paramètre un salaire mensuel et retourne le salaire hebdomadaire correspondant en divisant le salaire mensuel par 4.
+
+#    Créez une fonction appelée  salaire_horaire(salaire_hebdomadaire, heures_travaillees)  qui prend en paramètres un salaire hebdomadaire et le nombre d'heures travaillées par semaine, et retourne le salaire horaire correspondant en divisant le salaire hebdomadaire par le nombre d'heures travaillées par semaine.
+
+#    Demandez à l'utilisateur de saisir son salaire annuel.
+
+#    Demandez à l'utilisateur de saisir le nombre d'heures travaillées par semaine.
+
+#    Appelez les fonctions précédemment créées pour calculer le salaire horaire correspondant.
+
+#    Affichez le résultat sous la forme : "Votre salaire horaire est de XX euros".Ecrivez votre code ici
